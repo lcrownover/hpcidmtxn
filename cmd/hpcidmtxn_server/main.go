55d66c2db90e42df58d6d949c376d6e00572701b
Lucas Crownover <lcrownover127@gmail.com>
2022-11-08 16:21:36 -0800

    added uid to username lookup

--- cmd/hpcidmtxn_server/main.go
+++ cmd/hpcidmtxn_server/main.go
@@ -25,6 +25,16 @@ func GetLocalUIDFromUsername(username string) (int, error) {
 	return uid, nil
 }
 
+func GetLocalUsernameFromUID(uid int) (string, error) {
+	cmd := exec.Command("id", "-nu", fmt.Sprint(uid))
+	out, err := cmd.CombinedOutput()
+	if err != nil {
+		return "", err
+	}
+	username := strings.TrimSpace(string(out))
+	return username, nil
+}
+
 func main() {
 	router := gin.Default()
 
@@ -37,6 +47,21 @@ func main() {
 		}
 
 		c.String(http.StatusOK, "%s", fmt.Sprintf("%d", uid))
+	})
+
+	router.GET("/uid/:uid", func(c *gin.Context) {
+		param_uid := c.Param("uid")
+		uid, err := strconv.Atoi(param_uid)
+		if err != nil {
+			c.String(http.StatusBadRequest, "invalid integer: %s", fmt.Sprintf("%d", uid))
+		}
+
+		username, err := GetLocalUsernameFromUID(uid)
+		if err != nil {
+			c.String(http.StatusNotFound, "not found")
+		}
+
+		c.String(http.StatusOK, "%s", username)
 	})
 
 	router.Run(":8080")
